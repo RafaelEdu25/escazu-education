b8c6074fe1dbd2fb40096d55ca27f038b523c26d
leo <leonardo_franco@edupan.com>
2026-04-15 14:56:44 -0500

    fix(init): correct syntax for get-app command in init script

--- docker/init.sh
+++ docker/init.sh
@@ -25,7 +25,7 @@ sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
 bench get-app erpnext
-bench get-app --branch edupan https://github.com/DevOpsEdupan/escazu-education.git education
+bench get-app https://github.com/DevOpsEdupan/escazu-education --branch edupan
 
 bench new-site education.internal.edupan.dev \
 --force \
