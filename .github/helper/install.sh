14ff5d5c1050415a8c505a65517ec63a039fb9ac
leo <leonardo_franco@edupan.com>
2026-04-15 10:14:42 -0500

    feat(docker): update install script and add .env configuration for deployment

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -40,6 +40,8 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
+rm -rf ~/frappe-bench/apps/education ~/frappe-bench/apps/escazu-education
+
 bench get-app payments --branch ${BRANCH_TO_CLONE%"-hotfix"}
 bench get-app https://github.com/frappe/erpnext --branch "$BRANCH_TO_CLONE" --resolve-deps
 bench get-app education "${GITHUB_WORKSPACE}"
